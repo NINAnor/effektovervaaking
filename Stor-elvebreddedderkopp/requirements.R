83d53716b580348797da4f5aa1680f0e9fddf571
megsnowell <megan.nowell@nina.no>
2025-01-30 13:42:40 +0100

    stor-elvebreddedderkopp: requirements to execute pipeline

---
+++ Stor-elvebreddedderkopp/requirements.R
@@ -0,0 +1,14 @@
+#Install required packages for Stor-elvebreddedderkopp
+
+install.packages("readxl")
+install.packages("dplyr")
+install.packages("sf")
+install.packages("raster")
+install.packages("tidyr")
+install.packages("here")
+install.packages("DHARMa")
+install.packages("lme4")
+install.packages("MASS")
+install.packages("pscl")
+install.packages("ggplot2")
+install.packages("tidyverse")
